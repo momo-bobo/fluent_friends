9209acce46cee317cb0fb58d2da02156689622ef
momo-bobo <emaddyu@gmail.com>
2025-08-09 08:21:16 -0500

    Update tts_service_stub.dart

--- lib/services/tts_service_stub.dart
+++ lib/services/tts_service_stub.dart
@@ -2,4 +2,8 @@ class TtsService {
   Future<void> init() async {}
   Future<void> speak(String text) async {}
   Future<void> stop() async {}
+
+  // New API:
+  Future<List<String>> listVoices() async => <String>[];
+  Future<bool> setPreferredVoice(String name) async => false;
 }
