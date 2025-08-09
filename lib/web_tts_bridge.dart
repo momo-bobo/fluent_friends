2a5bb7dea124a3e73da312114cfe5bc26618bd7d
momo-bobo <emaddyu@gmail.com>
2025-08-09 08:12:27 -0500

    Update web_tts_bridge.dart

--- lib/web_tts_bridge.dart
+++ lib/web_tts_bridge.dart
@@ -8,3 +8,10 @@ external void speakText(String text);
 
 @JS('stopSpeaking')
 external void stopSpeaking();
+
+// New
+@JS('getVoiceNames')
+external List<dynamic> getVoiceNames();
+
+@JS('setPreferredVoiceByName')
+external bool setPreferredVoiceByName(String name);
