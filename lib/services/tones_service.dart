47e064c5f1302a9229b81b39a548c24517b80703
momo-bobo <emaddyu@gmail.com>
2025-08-09 11:25:25 -0500

    Update tones_service.dart

--- lib/services/tones_service.dart
+++ lib/services/tones_service.dart
@@ -1,15 +1,3 @@
 export 'tones_service_stub.dart'
   if (dart.library.html) 'tones_service_web.dart'
   if (dart.library.io) 'tones_service_mobile.dart';
-
-typedef MicLevelCallback = void Function(double level);
-
-class TonesService {
-  Future<void> init() async {}
-  Future<void> playStartDing() async {}
-  Future<void> playStopDing() async {}
-
-  // Web-only helpers (no-ops on mobile)
-  static Future<void> startMicLevelStream({required MicLevelCallback onLevel}) async {}
-  static Future<void> stopMicLevelStream() async {}
-}
