b11fffff73f7159a959c4497c0b6ef79ff24bdd1
momo-bobo <emaddyu@gmail.com>
2025-08-09 11:59:43 -0500

    Update tones_service_stub.dart

--- lib/services/tones_service_stub.dart
+++ lib/services/tones_service_stub.dart
@@ -1,10 +1,11 @@
-import 'tones_service.dart';
+typedef MicLevelCallback = void Function(double level);
 
 class TonesService {
   Future<void> init() async {}
+
   Future<void> playStartDing() async {}
   Future<void> playStopDing() async {}
 
-  static Future<void> startMicLevelStream({required MicLevelCallback onLevel}) async {}
-  static Future<void> stopMicLevelStream() async {}
+  Future<void> startMicLevelStream({required MicLevelCallback onLevel}) async {}
+  Future<void> stopMicLevelStream() async {}
 }
