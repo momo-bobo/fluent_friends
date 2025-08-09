41886ffe040c4dd298a34c561f6b0e1bebdea4f5
momo-bobo <emaddyu@gmail.com>
2025-08-09 11:38:24 -0500

    Update tones_service_web.dart

--- lib/services/tones_service_web.dart
+++ lib/services/tones_service_web.dart
@@ -1,8 +1,6 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-import 'dart:js' as js;
+typedef MicLevelCallback = void Function(double level);
+
 import 'package:js/js.dart';
-import 'tones_service.dart';
 
 @JS('playBeep')
 external void _playBeep(num freq, num durationMs);
@@ -17,20 +15,22 @@ class TonesService {
   Future<void> init() async {}
 
   Future<void> playStartDing() async {
-    _playBeep(880, 160); // higher ding
+    // Higher pitch, shorter
+    _playBeep(880, 160);
   }
 
   Future<void> playStopDing() async {
-    _playBeep(660, 180); // lower ding
+    // Lower pitch, slightly longer
+    _playBeep(660, 180);
   }
 
-  static Future<void> startMicLevelStream({required MicLevelCallback onLevel}) async {
+  Future<void> startMicLevelStream({required MicLevelCallback onLevel}) async {
     _startMicLevel(allowInterop((num level) {
       onLevel(level.toDouble());
     }));
   }
 
-  static Future<void> stopMicLevelStream() async {
+  Future<void> stopMicLevelStream() async {
     _stopMicLevel();
   }
 }
