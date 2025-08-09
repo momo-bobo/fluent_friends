50c412b9c03ca3c757255e8483a39762692e5808
momo-bobo <emaddyu@gmail.com>
2025-08-08 19:35:02 -0500

    Update animated_diagram.dart

--- lib/widgets/animated_diagram.dart
+++ lib/widgets/animated_diagram.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 
-class AnimatedDiagram extends StatelessWidget {
+class AnimatedDiagram extends StatefulWidget {
   final String sound;
   const AnimatedDiagram({super.key, required this.sound});
 
   @override
+  State<AnimatedDiagram> createState() => _AnimatedDiagramState();
+}
+
+class _AnimatedDiagramState extends State<AnimatedDiagram> with SingleTickerProviderStateMixin {
+  late final AnimationController _c;
+
+  @override
+  void initState() {
+    super.initState();
+    _c = AnimationController(vsync: this, duration: const Duration(seconds: 2))..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _c.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150, width: 150,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        borderRadius: BorderRadius.circular(10),
+    return ScaleTransition(
+      scale: Tween<double>(begin: 0.98, end: 1.02).animate(CurvedAnimation(parent: _c, curve: Curves.easeInOut)),
+      child: Container(
+        height: 150, width: 150,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 12, offset: const Offset(0, 6))],
+        ),
+        child: Text('👄\nMouth Diagram\n[${widget.sound}]', textAlign: TextAlign.center, style: const TextStyle(fontSize: 18)),
       ),
-      child: Text('👄\nMouth Diagram\n[$sound]', textAlign: TextAlign.center, style: const TextStyle(fontSize: 18)),
     );
   }
 }
